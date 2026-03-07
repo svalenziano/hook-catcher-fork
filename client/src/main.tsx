bdbb6f84c34ffdae93a6273eb7ebe06349af03f2
SValenziano <senorvalenz@gmail.com>
2026-03-07 14:51:28 -0500

    chore: trash the old Claude-assisted repo and replace with new human-configured one.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,20 @@
-import { StrictMode } from 'react';
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { BrowserRouter, Routes, Route } from "react-router"
 
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
+import "./index.css"
+import App from "./App.tsx"
+import { ThemeProvider } from "@/components/theme-provider.tsx"
 
-import App from '@/app/App';
-
-import './index.css';
-
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- root element is guaranteed by index.html
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/web/1234" element={"Basket 1234, wow!"} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>
-);
+)
