98058c71680d60f8891af4671a4dbfade9a1ad98
SValenziano <senorvalenz@gmail.com>
2026-03-09 20:17:19 -0400

    appearance tweaks

--- client/src/features/sv_sandbox/button-copy.tsx
+++ client/src/features/sv_sandbox/button-copy.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button"
-import { ClipboardCopy } from "lucide-react"
+import { Copy } from "lucide-react"
 
 export default function CopyButton({
   content,
@@ -16,7 +16,7 @@ export default function CopyButton({
       className={className}
       onClick={() => navigator.clipboard.writeText(content)}
     >
-      <ClipboardCopy />
+      <Copy />
     </Button>
   )
 }
